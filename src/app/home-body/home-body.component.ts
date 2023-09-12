f33657f06d54c2f49ba642082b2cd87eeb2feffc
Sentinel05 <p30sarkar@gmail.com>
2023-09-13 02:06:15 +0530

    Completed base project

--- src/app/home-body/home-body.component.ts
+++ src/app/home-body/home-body.component.ts
@@ -4,19 +4,37 @@ import { GeneralServiceService } from '../general-service.service';
 @Component({
   selector: 'app-home-body',
   templateUrl: './home-body.component.html',
-  styleUrls: ['./home-body.component.css']
+  styleUrls: ['./home-body.component.css'],
 })
 export class HomeBodyComponent {
-  a=3;
-  b=5;
-  ans=15;
+  // a=3;
+  // b=5;
+  // ans=15;
+  tasks: any = [];
 
   constructor(private dataService: GeneralServiceService) {
-    this.dataService.dummyFunc();
-    this.multiply(this.a, this.b);
+    // this.dataService.dummyFunc();
+    // this.multiply(this.a, this.b);
   }
 
-  multiply(a: number, b: number): number {
-    return a*b;
+  newTask(task: string) {
+    if (task != '') {
+      this.tasks.push({ id: this.tasks.length, title: task });
+      console.log(this.tasks);
+    }
   }
+
+  removeTask(id: number) {
+    this.tasks = this.tasks.filter((item: { id: number }) => {
+      item.id != id;
+    });
+  }
+
+  clearAll() {
+    this.tasks = [];
+  }
+
+  // multiply(a: number, b: number): number {
+  //   return a*b;
+  // }
 }
